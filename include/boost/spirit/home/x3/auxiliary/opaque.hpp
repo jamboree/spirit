2965be7b2988b8d6b2b65bdff222c57ccfbe2fb7
jamboree <tongari95@gmail.com>
2014-04-28 15:25:25 +0800

    improve opaque

--- include/boost/spirit/home/x3/auxiliary/opaque.hpp
+++ include/boost/spirit/home/x3/auxiliary/opaque.hpp
@@ -13,18 +13,37 @@
 
 #include <boost/spirit/home/x3/core/parser.hpp>
 #include <boost/spirit/home/x3/core/caller.hpp>
+#include <boost/spirit/home/x3/core/detail/transform_params.hpp>
 #include <boost/utility/result_of.hpp>
 
 
 namespace boost { namespace spirit { namespace x3
 {
     template <typename Gen>
-    struct opaque : parser<opaque<Gen>>, result_of<Gen const()>::type
+    struct opaque
+      : unary_parser<typename result_of<Gen const()>::type, opaque<Gen>>
     {
-        typedef typename result_of<Gen const()>::type base_type;
-        
+        typedef typename result_of<Gen const()>::type subject_type;
+        typedef unary_parser<subject_type, opaque<Gen>> base_type;
+        static bool const is_pass_through_unary = true;
+
         explicit opaque(Gen const& gen)
           : base_type(gen()) {}
+        
+        template <typename... Ts>
+        typename detail::transform_params<subject_type, void, Ts...>::sfinae_result
+        transform_params(Ts&&... ts) const
+        {
+            return this->subject.transform_params(std::forward<Ts>(ts)...);
+        }
+        
+        template <typename Iterator, typename Context, typename Attribute, typename... Ts>
+        bool parse(Iterator& first, Iterator const& last
+          , Context const& context, Attribute& attr, Ts&&... ts) const
+        {
+            return this->subject.parse(
+                first, last, context, attr, std::forward<Ts>(ts)...);
+        }
     };
     
     template <typename Gen>
@@ -36,36 +55,19 @@ namespace boost { namespace spirit { namespace x3
 
 namespace boost { namespace spirit { namespace x3 { namespace traits
 {
-    template <typename Gen, typename Context>
-    struct attribute_of<opaque<Gen>, Context>
-      : attribute_of<typename opaque<Gen>::base_type, Context>
-    {};
-    
-    template <typename Gen, typename Context>
-    struct has_attribute<opaque<Gen>, Context>
-      : has_attribute<typename opaque<Gen>::base_type, Context>
-    {};
-    
-    template <typename Gen, typename Context>
-    struct handles_container<opaque<Gen>, Context>
-      : handles_container<typename opaque<Gen>::base_type, Context>
-    {};
-    
-    // Caller
-    //--------------------------------------------------------------------------
     template <typename Gen, typename... Ts, typename Context>
     struct attribute_of<caller<opaque<Gen>, Ts...>, Context>
-      : attribute_of<caller<typename opaque<Gen>::base_type, Ts...>, Context>
+      : attribute_of<caller<typename opaque<Gen>::subject_type, Ts...>, Context>
     {};
     
     template <typename Gen, typename... Ts, typename Context>
     struct has_attribute<caller<opaque<Gen>, Ts...>, Context>
-      : has_attribute<caller<typename opaque<Gen>::base_type, Ts...>, Context>
+      : has_attribute<caller<typename opaque<Gen>::subject_type, Ts...>, Context>
     {};
     
     template <typename Gen, typename... Ts, typename Context>
     struct handles_container<caller<opaque<Gen>, Ts...>, Context>
-      : handles_container<caller<typename opaque<Gen>::base_type, Ts...>, Context>
+      : handles_container<caller<typename opaque<Gen>::subject_type, Ts...>, Context>
     {};
 }}}}
 
