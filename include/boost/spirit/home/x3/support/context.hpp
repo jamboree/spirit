56f4035592dde3e356bda6a16f65698c7005eec8
Jamboree <tongari95@gmail.com>
2014-05-14 00:37:42 +0800

    Revert "context collapse"
    This reverts commit fcf7077f550755cbc01f2de1a43e9703766b3a57.

--- include/boost/spirit/home/x3/support/context.hpp
+++ include/boost/spirit/home/x3/support/context.hpp
@@ -13,7 +13,6 @@
 #endif
 
 #include <boost/spirit/home/x3/support/unused.hpp>
-#include <boost/spirit/home/x3/support/utility/unrefcv.hpp>
 #include <boost/mpl/identity.hpp>
 
 namespace boost { namespace spirit { namespace x3
@@ -21,15 +20,13 @@ namespace boost { namespace spirit { namespace x3
     template <typename ID, typename T, typename Next = unused_type>
     struct context
     {
-        typedef unrefcv_t<Next> next_type;
-        
         context(T& val, Next const& next)
             : val(val), next(next) {}
 
         template <typename ID_, typename Unused = void>
         struct get_result
         {
-            typedef typename next_type::template get_result<ID_>::type type;
+            typedef typename Next::template get_result<ID_>::type type;
         };
 
         template <typename Unused>
@@ -38,47 +35,20 @@ namespace boost { namespace spirit { namespace x3
             typedef T& type;
         };
 
-        template <typename ID_, typename Unused = void>
-        struct remove_result
-        {
-            typedef typename
-                next_type::template remove_result<ID_>::type
-            removed_type;
-            typedef context<ID, T, removed_type> type;
-        };
-
-        template <typename Unused>
-        struct remove_result<mpl::identity<ID>, Unused>
-        {
-            typedef Next const& type;
-        };
-        
         T& get(mpl::identity<ID>) const
         {
             return val;
         }
 
         template <typename ID_>
-        typename get_result<ID_>::type
+        typename Next::template get_result<ID_>::type
         get(ID_ id) const
         {
             return next.get(id);
         }
-        
-        next_type const& remove(mpl::identity<ID>) const
-        {
-            return next;
-        }
-        
-        template <typename ID_>
-        typename remove_result<ID_>::type
-        remove(ID_ id) const
-        {
-            return {val, next.remove(id)};
-        }
 
         T& val;
-        Next next;
+        Next const& next;
     };
 
     template <typename ID, typename T>
@@ -102,41 +72,18 @@ namespace boost { namespace spirit { namespace x3
             typedef T& type;
         };
 
-        template <typename ID_, typename Unused = void>
-        struct remove_result
-        {
-            typedef context const& type;
-        };
-
-        template <typename Unused>
-        struct remove_result<mpl::identity<ID>, Unused>
-        {
-            typedef unused_type type;
-        };
-                
         T& get(mpl::identity<ID>) const
         {
             return val;
         }
 
         template <typename ID_>
-        unused_type get(ID_) const
+        unused_type
+        get(ID_) const
         {
             return unused;
         }
 
-        template <typename ID_>
-        unused_type remove(mpl::identity<ID>) const
-        {
-            return unused;
-        }
-        
-        template <typename ID_>
-        context const& remove(ID_) const
-        {
-            return *this;
-        }
-        
         T& val;
     };
 
@@ -148,11 +95,9 @@ namespace boost { namespace spirit { namespace x3
     }
 
     template <typename ID, typename T, typename Next>
-    inline context<ID, T, typename Next::
-        template remove_result<mpl::identity<ID>>::type>
-    make_context(T& val, Next const& next)
+    inline context<ID, T, Next> make_context(T& val, Next const& next)
     {
-        return {val, next.remove(mpl::identity<ID>())};
+        return context<ID, T, Next>(val, next);
     }
 
     template <typename ID, typename T>
